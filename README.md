ea4b4421fe049fd89d2498e0a29198095834f932
Dave <davidduartef@gmail.com>
2023-02-18 18:42:39 -0500

    update

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 
-# DrtDave Portfolio
-
-Hello there... this is my [portfolio](https://drt-dave.github.io/portfolio/) :D
+# My Portfolio
 
 
+
+[![portfolio2thumb](./media/portfoliothumb.png)](https://drt-dave.github.io/portfolio/) 
+
+>    Click on the image to visit the site 👆 
+
+
+
+## Installation
+
+1. Clone this repository to your local computer
+2. Open the index.html file in your web browser
+
+## Usage
+
+Simply click on the links to be redirected to the corresponding projects.
+## Contributing
+
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork this repository
+2. Create a branch with your changes: git checkout -b your-branch-name
+3. Make the necessary changes and commit: git commit -m "Description of your changes"
+4. Push your changes to your GitHub repository: git push origin your-branch-name
+5. Create a pull request to this repository.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for more details.
+Contact
+
+> If you have any questions or comments about this project, please contact me via email: davidduartef@gmail.com 
--- app.js
+++ app.js
@@ -6,50 +6,56 @@ const splitText = strText.split("");
 
 const originalText = text.textContent; // Store the original text of the element
 
-const myname= "DavidDuarte";
+const myname = "David Duarte";
 const splitName = myname.split("");
 
 let char = 0;
-let timer = null; 
+let timer = null;
+let firstClick = true;
 
-text.addEventListener("click",()=> {
-  if (text.textContent === originalText) {
-    text.textContent = "";
-    for (let i = 0; i < splitName.length; i++) {
-      text.innerHTML += "<span>" + splitName[i] + "</span>";
+
+
+text.addEventListener("click", () => {
+    if (firstClick) {
+        firstClick = false;
+        text.textContent = "";
+        for (let i = 0; i < splitName.length; i++) {
+            text.innerHTML += "<span>" + splitName[i] + "</span>";
+        }
+        timer = setInterval(onTick, 50);
+    } else if (text.textContent === originalText) {
+        //resetText();
+        //timer = setInterval(onTick, 50);
+        timer = null;
     }
-    timer = setInterval(onTick, 50);
-  } else {
-    resetText();
-    timer = setInterval(onTick, 50);
-  }
 });
 
-function onTick() {
-  const span = text.querySelectorAll('span')[char];
-  span.classList.add('drt');
-  char++;
 
-  if (char === splitName.length) {
-    complete();
-    return;
-  }
+function onTick() {
+    const spans = text.querySelectorAll('span');
+    if (char < spans.length) {
+        const span = spans[char];
+        span.classList.add('drt');
+        char++;
+    }
+    if (char === splitName.length) {
+        complete();
+        return;
+    }
 }
 
 function resetText() {
-  const spans = text.querySelectorAll('span');
-  spans.forEach(span => span.classList.remove('drt'));
-  char = 0;
-  setTimeout(() => {
-    text.textContent = originalText;
-  }, 200);
-}
-function complete() {
-  clearInterval(timer);
-  timer = null;
-  setTimeout(() => {
-    resetText();
-    timer = setInterval(onTick, 50);
-  }, 2000); // Wait 2 seconds before starting the animation again
+    char = 0;
+    setTimeout(() => {
+        text.textContent = originalText;
+    }, 2000);
 }
 
+function complete() {
+    clearInterval(timer);
+    timer = null;
+    firstClick = true;
+    setTimeout(() => {
+        resetText();
+    });
+}
